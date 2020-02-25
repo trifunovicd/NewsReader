af043cc111a0a36c9020132f23d9f6d35b42b222
Danijel Trifunovic <danijel.trifunovic@factory.hr>
2020-02-25 08:37:49 +0100

    Initial Commit

---
+++ NewsReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsReader
+//
+//  Created by Internship on 25/02/2020.
+//  Copyright © 2020 Internship. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
