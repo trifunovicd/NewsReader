32b2ddaa4edf2a3699ae62945c305b1560869012
Danijel Trifunović <danijel.trifunovic@factory.hr>
2020-03-20 14:49:41 +0100

    tests fix

--- NewsReaderTests/FavoritesViewModelTests.swift
+++ NewsReaderTests/FavoritesViewModelTests.swift
@@ -20,62 +20,52 @@ class FavoritesViewModelTests: QuickSpec {
             var sut: FavoritesViewModel!
             var disposeBag: DisposeBag!
             var shouldRefreshTable = false
-            var favorites: Results<Favorite>!
             
             beforeEach {
                 sut = FavoritesViewModel()
                 disposeBag = DisposeBag()
                 sut.bindFetchFavorites().disposed(by: disposeBag)
                 sut.setRemoveOption().disposed(by: disposeBag)
-            }
-            
-            
-            context("initial setup"){
-                it("should be an empty array of preview articles") {
-                    expect(sut.articlesPreview.value).to(beEmpty(), description: "Array must be empty!")
-                }
                 
-                it("should be an empty array of favorites") {
-                    expect(sut.favorites.value).to(beEmpty(), description: "Array must be empty!")
+                sut.refreshTable.subscribe(onNext: {
+                    shouldRefreshTable = true
+                }).disposed(by: disposeBag)
+                
+                Realm.Configuration.defaultConfiguration.inMemoryIdentifier = "testDatabase"
+                
+                do{
+                    let realm = try Realm()
+                    
+                    try realm.write {
+                      realm.deleteAll()
+                    }
+                }
+                catch{
+                    fail("Expecting to get failure")
                 }
             }
             
             
-            context("on fetching data"){
+            context("if database is empty") {
                 beforeEach {
                     sut.favoritesRequest.onNext(())
                 }
                 
-                it("should not throw error") {
-                    expect(sut.favoritesRequest).notTo(throwError())
+                it("should refresh table") {
+                    expect(shouldRefreshTable).to(beTrue(), description: "Table must be refreshed!")
                 }
                 
-                context("it should match saved favorites") {
-                    beforeEach {
-                        do{
-                            let realm = try Realm()
-                            favorites = realm.objects(Favorite.self).sorted(byKeyPath: "dateSaved", ascending: false)
-                        }
-                        catch{
-                            fail("Expecting to get failure")
-                        }
-                    }
-                    
-                    it("should get preview articles") {
-                        expect(sut.articlesPreview.value.count).to(equal(favorites.count))
-                    }
-                    
-                    it("should get all favorite articles") {
-                        expect(sut.favorites.value.count).to(equal(favorites.count))
-                    }
+                it("should show empty list of preview articles") {
+                    expect(sut.articlesPreview.value).to(beEmpty(), description: "Array must be empty!")
+                }
+                
+                it("should contain empty list of favorites") {
+                    expect(sut.favorites.value).to(beEmpty(), description: "Array must be empty!")
                 }
             }
             
-            
-            context("remove favorite") {
+            context("if database contains favorite articles") {
                 beforeEach {
-                    Realm.Configuration.defaultConfiguration.inMemoryIdentifier = "testDatabase"
-                    
                     let favorite = Favorite()
                     favorite.title = "testFavoriteTitle"
                     favorite.articleDescription = "testFavoriteDescription"
@@ -97,50 +87,36 @@ class FavoritesViewModelTests: QuickSpec {
                     }
                 }
                 
-                it("should add article to favorites") {
-                    do {
-                        let realm = try Realm()
-                        
-                        let savedArticle = realm.objects(Favorite.self).filter("url = 'testFavoriteUrl'")
-                        
-                        expect(savedArticle.first?.url) == "testFavoriteUrl"
-                        
-                        
-                    } catch  {
-                        fail("Expecting to get failure")
-                    }
-                }
-                
-                it("should remove article from favorites") {
-                    let articlePreview = ArticlePreview(title: "testFavoriteTitle", url: "testFavoriteUrl", urlToImage: "testFavoriteUrlToImage", isSelected: true)
-                    sut.removeFromFavorites.onNext(articlePreview)
-
-                    do {
-                        let realm = try Realm()
-
-                        let savedArticle = realm.objects(Favorite.self).filter("url = '\(articlePreview.url)'")
-
-                        expect(savedArticle).to(beEmpty())
-
-
-                    } catch  {
-                        fail("Expecting to get failure")
-                    }
-                }
-            }
-            
-            
-            context("refresh table") {
-                beforeEach {
-                    sut.refreshTable.subscribe(onNext: {
-                        shouldRefreshTable = true
-                    }).disposed(by: disposeBag)
-                    
-                    sut.favoritesRequest.onNext(())
-                }
-                
-                it("should refresh table on data fetched") {
+                it("should refresh table") {
                     expect(shouldRefreshTable).to(beTrue(), description: "Table must be refreshed!")
+                }
+                
+                it("should show preview of favorite articles") {
+                    expect(sut.articlesPreview.value.count).to(equal(1))
+                }
+                
+                it("should contain list of all favorite articles") {
+                    expect(sut.favorites.value.count).to(equal(1))
+                }
+                
+                
+                context("on remove favorite") {
+                    it("should remove article from favorites") {
+                        let articlePreview = ArticlePreview(title: "testFavoriteTitle", url: "testFavoriteUrl", urlToImage: "testFavoriteUrlToImage", isSelected: true)
+                        sut.removeFromFavorites.onNext(articlePreview)
+
+                        do {
+                            let realm = try Realm()
+
+                            let savedArticle = realm.objects(Favorite.self).filter("url = '\(articlePreview.url)'")
+
+                            expect(savedArticle).to(beEmpty())
+
+
+                        } catch  {
+                            fail("Expecting to get failure")
+                        }
+                    }
                 }
             }
         }
