1b438f7299a16af72c187522daab5942aa78390a
Danijel Trifunović <danijel.trifunovic@factory.hr>
2020-03-20 10:06:25 +0100

    FavoritesViewModelTests update

--- NewsReaderTests/FavoritesViewModelTests.swift
+++ NewsReaderTests/FavoritesViewModelTests.swift
@@ -11,6 +11,7 @@ import RxSwift
 import RxTest
 import Quick
 import Nimble
+import RealmSwift
 @testable import NewsReader
 
 class FavoritesViewModelTests: QuickSpec {
@@ -18,13 +19,16 @@ class FavoritesViewModelTests: QuickSpec {
         describe("FavoritesViewModel"){
             var sut: FavoritesViewModel!
             var disposeBag: DisposeBag!
+            var shouldRefreshTable = false
+            var favorites: Results<Favorite>!
             
             beforeEach {
                 sut = FavoritesViewModel()
                 disposeBag = DisposeBag()
-                
                 sut.bindFetchFavorites().disposed(by: disposeBag)
+                sut.setRemoveOption().disposed(by: disposeBag)
             }
+            
             
             context("initial setup"){
                 it("should be an empty array of preview articles") {
@@ -46,15 +50,99 @@ class FavoritesViewModelTests: QuickSpec {
                     expect(sut.favoritesRequest).notTo(throwError())
                 }
                 
-                it("should get preview articles") {
-                    expect(sut.articlesPreview.value.count).to(beGreaterThanOrEqualTo(0))
-                }
-                
-                it("should get all favorites articles") {
-                    expect(sut.favorites.value.count).to(beGreaterThanOrEqualTo(0))
+                context("it should match saved favorites") {
+                    beforeEach {
+                        do{
+                            let realm = try Realm()
+                            favorites = realm.objects(Favorite.self).sorted(byKeyPath: "dateSaved", ascending: false)
+                        }
+                        catch{
+                            fail("Expecting to get failure")
+                        }
+                    }
+                    
+                    it("should get preview articles") {
+                        expect(sut.articlesPreview.value.count).to(equal(favorites.count))
+                    }
+                    
+                    it("should get all favorite articles") {
+                        expect(sut.favorites.value.count).to(equal(favorites.count))
+                    }
                 }
             }
             
+            
+            context("remove favorite") {
+                beforeEach {
+                    Realm.Configuration.defaultConfiguration.inMemoryIdentifier = "testDatabase"
+                    
+                    let favorite = Favorite()
+                    favorite.title = "testFavoriteTitle"
+                    favorite.articleDescription = "testFavoriteDescription"
+                    favorite.url = "testFavoriteUrl"
+                    favorite.urlToImage = "testFavoriteUrlToImage"
+                    favorite.dateSaved = Date()
+                    
+                    do {
+                        let realm = try Realm()
+                        
+                        try realm.write {
+                            realm.add(favorite)
+                        }
+                        
+                        sut.favoritesRequest.onNext(())
+                        
+                    } catch  {
+                        fail("Expecting to get failure")
+                    }
+                }
+                
+                it("should add article to favorites") {
+                    do {
+                        let realm = try Realm()
+                        
+                        let savedArticle = realm.objects(Favorite.self).filter("url = 'testFavoriteUrl'")
+                        
+                        expect(savedArticle.first?.url) == "testFavoriteUrl"
+                        
+                        
+                    } catch  {
+                        fail("Expecting to get failure")
+                    }
+                }
+                
+                it("should remove article from favorites") {
+                    let articlePreview = ArticlePreview(title: "testFavoriteTitle", url: "testFavoriteUrl", urlToImage: "testFavoriteUrlToImage", isSelected: true)
+                    sut.removeFromFavorites.onNext(articlePreview)
+
+                    do {
+                        let realm = try Realm()
+
+                        let savedArticle = realm.objects(Favorite.self).filter("url = '\(articlePreview.url)'")
+
+                        expect(savedArticle).to(beEmpty())
+
+
+                    } catch  {
+                        fail("Expecting to get failure")
+                    }
+                }
+            }
+            
+            
+            context("refresh table") {
+                beforeEach {
+                    sut.refreshTable.subscribe(onNext: {
+                        shouldRefreshTable = true
+                    }).disposed(by: disposeBag)
+                    
+                    sut.favoritesRequest.onNext(())
+                }
+                
+                it("should refresh table on data fetched") {
+                    expect(shouldRefreshTable).to(beTrue(), description: "Table must be refreshed!")
+                }
+            }
         }
     }
 }
